71b2934407ec92d733ed208cacfb46608c857450
samhhenderson <yourname@example.com>
2022-09-19 11:05:32 -0700

    Complete styling. Complete adding and deleting book cards.

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ const submittedTitle = document.querySelector(`#submitted-title`);
 const submittedAuthor = document.querySelector(`#submitted-author`);
 const submittedPages = document.querySelector(`#submitted-pages`);
 const submittedReadStatus = document.querySelector(`#submitted-read-status`);
+const submitCancel = document.querySelector(`#submit-cancel`);
+const pageMask = document.querySelector(`#page-mask`);
 
 const textInputs = document.querySelectorAll(`input`);
+
 
 let myLibrary = [];
 
@@ -25,53 +28,72 @@ function bookMaker(title, author, pages, readStatus) {
   }
 
 function showBookForm() {
+    formContainer.reset();
+    submitBookButton.disabled = true;
     formContainer.style.display = `grid`;
+    pageMask.style.display = `block`;
 }
 
+//deletes all cards and recreates with library array as currently
+//constituted
 function cardMaker() {
     //preemptively delete all book cards so they can be recreated
     document.querySelectorAll(".book-card").forEach(bookCard => 
             bookCard.remove());
-
+    let bookHtml = ``;
+    
     myLibrary.forEach((book, index) => {
 
-        console.log(book);
+        let checkedStatus = ``;
+        if (book.readStatus == true) checkedStatus = `checked`;
 
-        let bookCard = document.createElement(`div`);
-        bookCard.setAttribute(`class`, `book-card`);
-        bookCard.setAttribute(`id`, index);
-        bookCard.style.backgroundColor = `white`;
-        bookCard.style.border = `2px solid red`;
-        bookCard.style.borderRadius = `10px`;
-        bookCard.style.width = `200px`;
-        bookCard.style.height = `200px`;
-        bookCard.style.display = `grid`
+        bookHtml +=
+       `<div class="book-card card" id="book-${index}">
+            <label for="book-title-${index}">Title:
+                <h3 id="book-title-${index}">${book.title}</h3>
+            </label>
+            <label for="book-author-${index}">Author:
+                <h4 id="book-author-${index}">${book.author}</h3>
+            </label>
+            <label class="horiz page-count" for="book-page-count-${index}">
+                    Page Count:
+                <h4 id="book-page-count-${index}">${book.pages}</h3>
+            </label>
+            <label class="horiz" for="submitted-read-status-${index}">READ IT?
+            <input class="checkbox-input book-read-status" type="checkbox" 
+                id="submitted-read-status-${index}" ${checkedStatus}>
+            </label>
+            <button class="book-delete light x-button" 
+                id="book-delete-${index}">+</button>
+        </div>`
 
-        bookContainer.appendChild(bookCard);
+    });
+    bookContainer.innerHTML = bookHtml;
 
-        let bookTitle = document.createElement(`div`);
-        bookTitle.innerHTML = book.title;
-        bookCard.appendChild(bookTitle);
+    formContainer.style.display = `none`;
+    pageMask.style.display = `none`;
 
-        let bookAuthor = document.createElement(`div`);
-        bookAuthor.innerHTML = book.author;
-        bookCard.appendChild(bookAuthor);
+    //add event listeners to all new delete and READ buttons/checkboxes
+    let bookDeleteButtons = document.querySelectorAll(`.book-delete`);
+    bookDeleteButtons.forEach(bookDeleteButton => {
+        bookDeleteButton.addEventListener(`click`, (event) => 
+                deleteBook(event));
+    });
 
-        let bookPages = document.createElement(`div`);
-        bookPages.innerHTML = book.pages;
-        bookCard.appendChild(bookPages);
-
-        //DO NEXT: use template creation to add new books
-
+    let readStatusBoxes = document.querySelectorAll(`.book-read-status`);
+    readStatusBoxes.forEach(readStatusBox => {
+        readStatusBox.addEventListener(`click`, (event) => 
+                changeReadStatus(event));
     });
 }
 
+//add new information to book item in array
 function addBookToList() {
     myLibrary.push(new bookMaker(
         submittedTitle.value,
         submittedAuthor.value,
         submittedPages.value,
-        submittedReadStatus.value
+        submittedReadStatus.checked
     ));
     cardMaker();
 }
@@ -80,8 +102,18 @@ function addBookToList() {
 //check if fields have values before enabling submit button
 function enableSubmitButton() {
     submitBookButton.disabled = !(submittedTitle.value && 
-            submittedAuthor.value && submittedPages.value && 
-            submittedReadStatus.value);
+            submittedAuthor.value && submittedPages.value);
+}
+
+function deleteBook (event) {
+    let targetIndex = event.target.getAttribute(`id`).slice(12);
+    myLibrary.splice(targetIndex,1);
+    cardMaker();
+} 
+
+function changeReadStatus(event) {
+    let targetIndex = event.target.getAttribute(`id`).slice(22);
+    myLibrary[targetIndex].readStatus = !myLibrary[targetIndex].readStatus;
 }
 
 //EVENT LISTENERS - Nothing happens until things get clicked
@@ -90,3 +122,10 @@ submitBookButton.addEventListener('click', addBookToList);
 textInputs.forEach(textInput => {
     textInput.addEventListener(`input`, enableSubmitButton);
 });
+
+//Close new book form and hide when x is clicked
+submitCancel.addEventListener(`click`, () => {
+    formContainer.style.display = `none`;
+    pageMask.style.display = `none`;
+})
+
